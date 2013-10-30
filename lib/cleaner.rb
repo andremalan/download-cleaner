a3091ec83596b9f40590e26f65902176a590fee6
Andre Malan <andre@redroverhq.com>
2013-10-30 18:04:19 -0400

    cleaner works

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -1,1 +1,16 @@
-class Cleaner; end
+class Cleaner
+
+ def initialize path
+   @path = path
+ end
+
+ def clean!
+   entries = Dir.entries(@path)
+   entries.each do |entry|
+     if entry.match /\.dmg/
+       File.delete(@path + entry)
+     end
+   end
+ end
+
+end
--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,8 +1,38 @@
 require 'rspec'
+require 'cleaner'
+require 'fileutils'
 
 describe Cleaner do
-  describe "clean" do
-    it "removes all the dmg files in the downloads directory"
-    it "does not remove other files in the downloads directory"
+  let(:path){"spec/fixtures/"}
+
+  subject {Cleaner.new(path)}
+
+  before(:each) do
+      FileUtils.touch(path + "testapp.dmg")
+      FileUtils.touch(path + "testapp.not_dmg")
+  end
+
+  after(:each) do
+    Dir.entries(path).each do |entry|
+      if entry != "."
+        File.delete(path + entry)
+      end
+    end
+
+  end
+  describe "clean!" do
+    it "removes all the dmg files in the downloads directory" do
+
+      subject.clean!
+      entries = Dir.entries(path)
+      entries.should_not include('testapp.dmg')
+
+
+    end
+    it "does not remove other files in the downloads directory" do
+      entries = Dir.entries(path)
+      entries.should include('testapp.not_dmg')
+
+    end
   end
 end
